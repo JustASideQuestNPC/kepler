51b5ae81cdc0c660fde7aca50f90f6f51f2b8a7d
Joe Williams <joe.williams@oit.edu>
2023-12-22 11:57:04 -0800

    converted the debug sandbox into a demo

---
+++ demos/collider/main.js
@@ -0,0 +1,139 @@
+/** @type {KInput} */
+let input;
+
+/** @type {PointCollider} */
+let staticPoint;
+/** @type {LineCollider} */
+let staticLine;
+/** @type {CircleCollider} */
+let staticCircle;
+/** @type {PolygonCollider} */
+let staticPolygon;
+/** @type {Collider[]} */
+let dynamicColliders = [];
+/** @type {Collider} */
+let currentDynamicCollider;
+/** @type {number} */
+let colliderIndex = 0;
+
+function preload() {
+  loadJSON("../../extras/color-palette.json", loadPalette);
+}
+
+/* setup runs once once at runtime start */
+function setup() {
+  createCanvas(800, 800);
+  input = KInput.makeNew(window);
+  input.addAction({
+    name: "cycle colliders",
+    keys: [Key.LEFT_MOUSE],
+    mode: PRESS,
+    callback: () => {
+      colliderIndex = (colliderIndex + 1) % dynamicColliders.length;
+      currentDynamicCollider = dynamicColliders[colliderIndex];
+    }
+  });
+
+  staticPoint = new PointCollider(width / 4, height / 4);
+
+  staticLine = new LineCollider(
+    (width * 3 / 4) - 50, (height / 4) - 50,
+    (width * 3 / 4) + 50, (height / 4) + 50
+  );
+
+  staticCircle = new CircleCollider(width / 4, height * 3 / 4, 75);
+
+  staticPolygon = new PolygonCollider([
+    [0, -114],
+    [96, -52],
+    [96, 52],
+    [0, 114],
+    [-96, 52],
+    [-96, -52]], 
+    width * 3 / 4, height * 3 / 4
+  );
+
+  dynamicColliders[0] = new PointCollider(0, 0);
+  dynamicColliders[1] = new LineCollider(-50, -30, 50, 30);
+  dynamicColliders[2] = new CircleCollider(0, 0, 40);
+  dynamicColliders[3] = new PolygonCollider([
+    [-50, -20],
+    [30, 0],
+    [-20, 40]
+  ]);
+
+  currentDynamicCollider = dynamicColliders[0];
+}
+
+/* draw runs once per frame */
+function draw() {
+  input.update();
+
+  currentDynamicCollider.setPos(mouseX, mouseY);
+  staticPolygon.modAngle(0.01);
+
+  background(WHITE);
+
+  noFill();
+  strokeWeight(5);
+  let colliding = false;
+
+  if (currentDynamicCollider.isColliding(staticPoint)) {
+    stroke(RED);
+    colliding = true;
+  }
+  else {
+    stroke(GREEN);
+  }
+  staticPoint.render();
+
+  if (currentDynamicCollider.isColliding(staticLine)) {
+    stroke(RED);
+    colliding = true;
+  }
+  else {
+    stroke(GREEN);
+  }
+  staticLine.render();
+
+  let hasMTV = false;
+  let transVec = createVector(0, 0);
+  if (currentDynamicCollider.isColliding(staticCircle, transVec)) {
+    stroke(RED);
+    colliding = true;
+    hasMTV = true;
+  }
+  else {
+    stroke(GREEN);
+  }
+  staticCircle.render();
+
+  if (currentDynamicCollider.isColliding(staticPolygon, transVec)) {
+    stroke(RED);
+    colliding = true;
+    hasMTV = true;
+  }
+  else {
+    stroke(GREEN);
+  }
+  staticPolygon.render();
+
+  if (colliding) stroke(RED);
+  else stroke(GREEN);
+  currentDynamicCollider.render();
+
+  // for some reason this triggers even if there's no MTV and hasMTV is false??
+  // i'm not worrying about it tho, i've got too many other bugs to stomp :/
+  if (colliding && hasMTV) {
+    stroke(BLUE);
+    currentDynamicCollider.modPos(transVec);
+    currentDynamicCollider.render();
+  }
+
+  noStroke();
+  fill(BLACK);
+  textSize(16);
+  textAlign(LEFT, TOP);
+  text(`MTV: (${Math.floor(transVec.x)}, ${Math.floor(transVec.y)})`, 5, 5);
+}
+
